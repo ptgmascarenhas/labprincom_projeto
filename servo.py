a1e9b087cc562affda317e01dd33f58f36fe66c1
Paulo Mascarenhas <paulo.thiago.98@gmail.com>
2019-10-01 15:29:27 -0300

    mudanca no servo

--- servo.py
+++ servo.py
@@ -1,35 +1,23 @@
 import RPi.GPIO as GPIO
-import time
- 
-servo_pin = 18
- 
-#Ajuste estes valores para obter o intervalo completo do movimento do servo
-deg_0_pulse   = 0.5 
-deg_180_pulse = 2.5
-f = 50.0
- 
-# Faca alguns calculos dos parametros da largura do pulso
-period = 1000/f
-k      = 100/period
-deg_0_duty = deg_0_pulse*k
-pulse_range = deg_180_pulse - deg_0_pulse
-duty_range = pulse_range * k
- 
-#Iniciar o pino gpio
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servo_pin,GPIO.OUT)
-pwm = GPIO.PWM(servo_pin,f)
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(03, GPIO.OUT)
+
+pwm=GPIO.PWM(03, 50)
+
 pwm.start(0)
- 
-def set_angle(angle):
-        duty = deg_0_duty + (angle/180.0)* duty_range
-        pwm.ChangeDutyCycle(duty)
- 
-try:
-        while True:
-                angle = input ("Enter angle (0 a 180): ")
-                set_angle(angle)
- 
-finally:
-        print("cleaning up")
-        GPIO.cleanup()
+
+def SetAngle(angle):
+
+	duty = angle / 18 + 2
+	GPIO.output(03, True)
+	pwm.ChangeDutyCycle(duty)
+	sleep(1)
+	GPIO.output(03, False)
+	pwm.ChangeDutyCycle(0)
+
+SetAngle(90) 
+
+pwm.stop()
+GPIO.cleanup()
