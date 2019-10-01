ae383b9d645b62aa1cab41373fb5d9c7af83c18a
Paulo Mascarenhas <paulo.thiago.98@gmail.com>
2019-10-01 15:46:01 -0300

    testes no servo

--- servo.py
+++ servo.py
@@ -1,26 +1,30 @@
 import RPi.GPIO as GPIO
-from time import sleep
-import sys
+import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(03, GPIO.OUT)
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
 
-pwm=GPIO.PWM(03, 50)
-
-pwm.start(0)
-
-def SetAngle(angle):
-
-	duty = angle / 18 + 2
-	GPIO.output(03, True)
-	pwm.ChangeDutyCycle(duty)
-	sleep(1)
-	GPIO.output(03, False)
-	pwm.ChangeDutyCycle(0)
-
-for i in range(sys.argv[1], sys.argv[2]):
-    SetAngle(i)
-    sleep(0.1) 
-
-pwm.stop()
-GPIO.cleanup()
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(2.5) # Initialization
+try:
+  while True:
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(12.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(2.5)
+    time.sleep(0.5)
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
