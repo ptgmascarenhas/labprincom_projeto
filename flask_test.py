70970d6b1b35e6df0d8b2199d53bf749a94bb805
Paulo Mascarenhas <paulo.thiago.98@gmail.com>
2019-10-07 21:59:40 -0300

    teste com valor do sensor no flask

--- flask_test.py
+++ flask_test.py
@@ -11,7 +11,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return 'teste'
+    dst = sensor.get_distance()
+    return str(dst)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True,port=8080)
