a38654afde0e8c0e9c1a2f1695d00129e833276c
Paulo Mascarenhas <paulo.thiago.98@gmail.com>
2019-10-07 21:32:13 -0300

    teste na classe

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import RPi.GPIO as GPIO
-import ultrasonic_sensor
+import HC_SR04
 import time
 
 
-try:
-    while True:
-        dist = ultrasonic_sensor.get_distance()
-        print ("Measured Distance = %.1f cm" % dist)
-        time.sleep(1)
- 
-except KeyboardInterrupt:
-    print("Stoping...")
-    GPIO.cleanup()
+sensor = HC_SR04()
+
+sensor.get_distance()
