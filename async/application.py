aded8545636553c926653972a77ee238da4ceeeb
Paulo Mascarenhas <paulo.thiago.98@gmail.com>
2019-10-15 22:54:54 -0300

    .

--- async/application.py
+++ async/application.py
@@ -1,4 +1,3 @@
-# Start with a basic flask app webpage.
 from flask_socketio import SocketIO, emit
 from flask import Flask, render_template, url_for, copy_current_request_context
 from random import random
@@ -24,26 +23,19 @@ GPIO.setmode(GPIO.BCM)
 
 sensor = HC_SR04()
 
-class RandomThread(Thread):
+class Distance(Thread):
     def __init__(self):
         self.delay = 1
-        super(RandomThread, self).__init__()
+        super(Distance, self).__init__()
 
-    def randomNumberGenerator(self):
-        """
-        Generate a random number every 1 second and emit to a socketio instance (broadcast)
-        Ideally to be run in a separate thread?
-        """
-        #infinite loop of magical random numbers
-        print("Making random numbers")
+    def measure(self):
         while not thread_stop_event.isSet():
             dst = sensor.get_distance()
             socketio.emit('newnumber', {'number': dst}, namespace='/test')
             sleep(self.delay)
 
     def run(self):
-        self.randomNumberGenerator()
-
+        self.measure()
 
 @app.route('/')
 def index():
@@ -59,13 +51,12 @@ def test_connect():
     #Start the random number generator thread only if the thread has not been started before.
     if not thread.isAlive():
         print("Starting Thread")
-        thread = RandomThread()
+        thread = Distance()
         thread.start()
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print('Client disconnected')
-
 
 if __name__ == '__main__':
     socketio.run(app.run(host='0.0.0.0',debug=True,port=8080))
