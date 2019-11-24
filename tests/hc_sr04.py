6e287fe3bab33acf5cfd9a5e965aff3c90241f20
Paulo <paulo.thiago.98@gmail.com>
2019-11-24 12:47:39 -0200

    retirando assincrono

--- tests/hc_sr04.py
+++ tests/hc_sr04.py
@@ -1,7 +1,41 @@
-import random
+import RPi.GPIO as GPIO
+import time
 
 class HC_SR04():
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        
+        #set GPIO Pins
+        self.GPIO_TRIGGER = 24
+        self.GPIO_ECHO = 23
+        
+        #set GPIO direction (IN / OUT)
+        GPIO.setup(self.GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(self.GPIO_ECHO, GPIO.IN)
+
     def get_distance(self):
-        distance = random.randint(0, 300)
+        # set Trigger to HIGH
+        GPIO.output(self.GPIO_TRIGGER, True)
+    
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(self.GPIO_TRIGGER, False)
+    
+        StartTime = time.time()
+        StopTime = time.time()
+    
+        # save StartTime
+        while GPIO.input(self.GPIO_ECHO) == 0:
+            StartTime = time.time()
+    
+        # save time of arrival
+        while GPIO.input(self.GPIO_ECHO) == 1:
+            StopTime = time.time()
+    
+        # time difference between start and arrival
+        TimeElapsed = StopTime - StartTime
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = (TimeElapsed * 34300) / 2
     
         return distance
