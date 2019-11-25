67e8f480c08b91f4e9cef3177c1ce3c1232c20bb
Paulo Mascarenhas <paulo.thiago.98@gmail.com>
2019-11-25 16:43:55 -0300

    retorno da imagem

--- flask_test.py
+++ flask_test.py
@@ -1,19 +1,13 @@
 from flask import Flask, render_template, request, send_file
-from hc_sr04 import HC_SR04
-import time
-import random
-from time import sleep
-from plot_graph import get_graph
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
 	return "Vá para /mapa"
-    
+
 @app.route("/mapa")
 def mapear():	
-	get_graph()
 	return render_template("index_image.html")
 
 if __name__ == "__main__":
